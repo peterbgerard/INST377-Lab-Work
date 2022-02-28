6069d71240c9f6d19e96dc6ccae762dee61a74ba
Peter G <peterbennnettgerard@gmail.com>
2022-02-27 19:13:19 -0500

    lab 4 work

---
+++ client/lab_4/script.js
@@ -0,0 +1,48 @@
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+document
+    .querySelector('#carousel-button-next')
+        .addEventListener("click", function() {
+            moveToNextSlide(); 
+        });
+
+document
+    .querySelector('#carousel-button-prev')
+        .addEventListener("click", function() {
+            moveToPrevSlide(); 
+        });
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel-item-visible')
+}
+
+function moveToNextSlide() {
+    updateSlidePosition() 
+
+    if (slidePosition == totalSlides - 1) {
+        slidePosition = 0;
+    }
+    else 
+    {
+        slidePosition++;
+    }
+}
+
+function moveToPrevSlide() {
+    updateSlidePosition() 
+
+    if (slidePosition == 0) {
+        slidePosition = totalSlides - 1;
+    }
+    else 
+    {
+        slidePosition--;
+    }
+}
