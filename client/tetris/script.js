d9e375c7af3e6fbd4f3b9af22d2318e37f9e438e
Peter G <peterbennnettgerard@gmail.com>
2022-03-08 23:33:21 -0500

    tetris movedown test

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -64,4 +64,14 @@ const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino')
         })
+    };
+
+    //make the tetromino move down every second
+    timerId = setInterval(moveDown , 1000)
+
+    //move down function
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
     }
