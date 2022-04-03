b499beb3a0c78e927b89b0d78030bc3874f064aa
Peter G <peterbennnettgerard@gmail.com>
2022-04-03 19:46:26 -0400

    lab 6 finishing touches

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,41 +1,59 @@
-function getRandomIntInclusive(min, max) {
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  return Math.floor(
-    Math.random() * (newMax - newMin + 1) + newMin
-  ); // The maximum is inclusive and the minimum is inclusive
-}
+function dataHandler(array) {
+    // Shuffle array - Googled this
+    let shuffled = array
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
 
-// TODO: Use range to return random item from collection/array 
-function dataHandler (dataArray) {
-  console.log('fired dataHandler');
-  console.table(dataArray); // this is called "dot notation"
-  const range = [...Array(15).keys()];
-  range.forEach((item) => {
-    console.log('range item', item);
-  });
-}
+    // Get sub-array of first 15 elements after shuffled
+    let selected = shuffled.slice(0, 15);
 
-// As the last step of your lab, hook this up to index.html
+    return selected
+  }
+  
+  // Add the contents of options[0] to #foo:
+  //document.getElementById('foo').appendChild(makeUL(options[0]));
+
 async function mainEvent() { // the async keyword means we can make API requests
-  console.log('script loaded'); // this is substituting for a "breakpoint"
-  const form = document.querySelector('.main_form');
-  const submit = document.querySelector('.submit_button');
-  submit.style.display = 'none'; // it's better not to display this until the data has loaded
+  const form = document.querySelector('.main_form')
+  const submit = document.querySelector('.button');
+  submit.style.setProperty('display', 'none');
 
-  const results = await fetch('/api/foodservicesPG'); // This accesses some data from our API
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  console.log(arrayFromJson);
-  if (arrayFromJson.data.length > 0) {
+  
+  // get a list of 15 random restaurant names from the data
+  rest_names = dataHandler(arrayFromJson);  
+  
+  console.log(rest_names)
+
+  document.getElementById('resto-list').innerHTML = rest_names.map(item => `<li>${ item['name'] }</li>`).join('');
+    
+  // don't show the button until the data is ready
+  if (arrayFromJson.length > 0) {
+    submit.style.setProperty('display', 'block');
+    
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
-      console.log('form submission'); // this is substituting for a "breakpoint"
-      submit.style.display = 'block';  
+      // console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      dataHandler(arrayFromJson.data);
-    });
+    })
   }
-  // this actually runs first! It's calling the function above
-  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
 }
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+
+/*
+
+Using dot notation and template strings (`this ${variable} kind`} to build some HTML strings, 
+then use innerHTML inject your collection of random restaurants into the list you made in step 13.
+A CodePen example of dot notation vs bracket notation (Links to an external site.)
+ES6 - How to write strings with variables included (Links to an external site.)
+You'll need to have the unordered list as a target, so don't forget your querySelector()
+The list should refresh with different content every time your form is submitted, 
+ie: it should change when you hit the submit button.
+The new list should replace the old one entirely, not append.
+ 
+*/
