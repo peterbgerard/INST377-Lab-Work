06e1f4f25d39e24cba03a4c3a18ad33488852681
Peter G <peterbennnettgerard@gmail.com>
2022-04-03 19:31:02 -0400

    lab 6 tests

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,15 +1,41 @@
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  ); // The maximum is inclusive and the minimum is inclusive
+}
+
+// TODO: Use range to return random item from collection/array 
+function dataHandler (dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('range item', item);
   });
 }
 
-// this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+// As the last step of your lab, hook this up to index.html
+async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded'); // this is substituting for a "breakpoint"
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('.submit_button');
+  submit.style.display = 'none'; // it's better not to display this until the data has loaded
+
+  const results = await fetch('/api/foodservicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+  if (arrayFromJson.data.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      submit.style.display = 'block';  
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      dataHandler(arrayFromJson.data);
+    });
+  }
+  // this actually runs first! It's calling the function above
+  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+}
